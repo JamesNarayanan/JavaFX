a220f425d8df00157298577dcda76a9a2a7e8c6e
203640 <203640@STUDSK19-117.whrhs.org>
2018-12-06 20:57:33 -0500

    Undo School Test

--- Snake/src/SnakeGame.java
+++ Snake/src/SnakeGame.java
@@ -29,7 +29,7 @@ public class SnakeGame extends Application {
 		mainStage.show();
 	}
 	
-	private Scene mainScene() { //Added from schools
+	private Scene mainScene() {
 		Grid grid = new Grid(this);
 		grid.newSnake();
 		
